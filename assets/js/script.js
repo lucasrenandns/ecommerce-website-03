7d1c38d17f481fc0a6aee99621737eb6f3859127
lucasrenandns <lucasrenandns96@gmail.com>
2023-01-07 19:09:19 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 const navegation = document.querySelector(".navegation")
+const scrollArrow = document.getElementById("scroll-arrow")
 
 window.addEventListener("scroll", function() {
     navegation.classList.toggle("sticky", window.scrollY > 0)
 })
+
+scrollArrow.addEventListener("click", function() {
+    window.scrollTo(0, 0)   
+})
+
+function hiddenScrollArrow() {
+    if(window.scrollY < 500) {
+        scrollArrow.style.display = "none"
+    }
+    else {
+        scrollArrow.style.display = "inline-flex"
+    }
+}
+window.addEventListener("scroll", hiddenScrollArrow)
+hiddenScrollArrow()
