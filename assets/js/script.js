bbd1bc53c3c38fad17b62b92bc3c9b5130c58b68
lucasrenandns <lucasrenandns96@gmail.com>
2023-01-08 14:17:04 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const navegation = document.querySelector(".navegation")
 const scrollArrow = document.getElementById("scroll-arrow")
+const menuIcon = document.getElementById("menu-icon")
+const navBar = document.querySelector(".navbar")
 
 window.addEventListener("scroll", function() {
     navegation.classList.toggle("sticky", window.scrollY > 0)
@@ -7,6 +9,11 @@ window.addEventListener("scroll", function() {
 
 scrollArrow.addEventListener("click", function() {
     window.scrollTo(0, 0)   
+})
+
+menuIcon.addEventListener("click", function() {
+    navBar.classList.toggle("open")
+    menuIcon.classList.toggle("bx-x")
 })
 
 function hiddenScrollArrow() {
@@ -17,5 +24,10 @@ function hiddenScrollArrow() {
         scrollArrow.style.display = "inline-flex"
     }
 }
-window.addEventListener("scroll", hiddenScrollArrow)
+
+window.addEventListener("scroll", function() {
+    hiddenScrollArrow()
+    navBar.classList.remove("open")
+    menuIcon.classList.remove("bx-x")
+})
 hiddenScrollArrow()
